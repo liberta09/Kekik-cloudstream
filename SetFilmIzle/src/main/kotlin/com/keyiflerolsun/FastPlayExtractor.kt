335f601d58d717eeb19c16e2cc186be851f04e6f
liberta09 <kaanokan258@gmail.com>
2026-08-26 15:36:59 +0300

    Restore SetFilmIzle plugin

---
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/FastPlayExtractor.kt
@@ -0,0 +1,8 @@
+package com.keyiflerolsun
+
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.Filemoon
+
+class FastPlayExtractor : Filemoon() {
+    override var mainUrl = "https://fastplay.to"
+}
