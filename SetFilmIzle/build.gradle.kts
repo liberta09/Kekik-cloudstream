fb688c3934f91edab5a57c2500fca68d7801e590
liberta09 <kaanokan258@gmail.com>
2026-08-26 15:37:50 +0300

    Restore original SetFilmIzle configuration

--- SetFilmIzle/build.gradle.kts
+++ SetFilmIzle/build.gradle.kts
@@ -1,11 +1,18 @@
-plugins {
-    id("com.lagradost.cloudstream3.gradle")
-}
+version = 30
 
 cloudstream {
-    language =  "tr"
-    description = "SetFilmIzle eklentisi"
-    authors = listOf("liberta09")
-    status = 1
-    tvTypes = listOf("TvType.Movie", "TvType.TvSeries")
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "Setfilmizle sitemizde, donma yaşamadan Türkçe dublaj ve altyazılı filmleri ile dizileri muhteşem 1080p full HD kalitesinde izleyebilirsiniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.setfilmizle.uk&sz=%size%"
 }
