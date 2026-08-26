be0738bcc467da193c082340a1d8926e5e7c63e0
liberta09 <kaanokan258@gmail.com>
2026-08-26 15:37:11 +0300

    Restore SetFilmIzle plugin

---
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetPlayExtractor.kt
@@ -0,0 +1,8 @@
+package com.keyiflerolsun
+
+import com.lagradost.cloudstream3.extractors.GenericExtractor
+
+class SetPlayExtractor : GenericExtractor() {
+    override val name = "SetPlay"
+    override var mainUrl = "https://setplay.net"
+}
