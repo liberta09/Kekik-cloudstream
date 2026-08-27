befd559c7562571401f121d32c56e9772eb6a61b
liberta09 <kaanokan258@gmail.com>
2026-08-27 13:48:17 +0300

    Klonla Anizm eklentisini nik-cloudstream'den

---
+++ Anizm/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    authors = listOf("csprofesor")
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=anizm.net&sz=%size%"
+}
